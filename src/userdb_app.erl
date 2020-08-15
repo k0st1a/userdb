dc9a252ed0cf61ecb00fec1188fa7762b8b9058f
Konstantin Malikov <konstantin.malikov@eltex.loc>
2020-08-15 23:05:33 +0700

    Создание скелета приложения

---
+++ src/userdb_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc userdb public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(userdb_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    userdb_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
